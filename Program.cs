08f37ccde1d18546608abc62a125058575070da5
Usuario1 <Usuario1@DESKTOP-LAS9AIG>
2023-11-05 14:48:50 -0500

    Modelos y configuración del DbContext#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SupermarketWEB.Data;
+
+
 namespace SupermarketWEB
 {
     public class Program
@@ -8,6 +13,10 @@ namespace SupermarketWEB
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<SupermarketContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB"))
+            );
 
             var app = builder.Build();
 
