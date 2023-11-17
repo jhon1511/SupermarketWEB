ce03db0da3c98e396ad56482941ea9210198a7ed
Usuario1 <Usuario1@DESKTOP-LAS9AIG>
2023-11-16 21:29:47 -0500

    corregido_error_product#

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,8 +11,8 @@ namespace SupermarketWEB.Models
 
         public decimal Price { get; set; }
         public int Stock {  get; set; }
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
 
 
     }
